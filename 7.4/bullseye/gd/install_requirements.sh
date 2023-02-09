13c454b4e80e2d90751c132deb912fb980bc9b11
Wouter Schreurs <wouter@etrias.nl>
2023-02-09 14:40:09 +0100

    Update install_requirements.sh

--- 7.4/bullseye/gd/install_requirements.sh
+++ 7.4/bullseye/gd/install_requirements.sh
@@ -4,6 +4,7 @@ apt-get update && \
 apt-get install -y --no-install-recommends \
 libfreetype6 \
 libpng16-16=1.6.37-3 \
+libwebp-dev \
 libjpeg62-turbo=1:2.0.6-4 && \
 rm -rf /var/lib/apt/lists/*
 
